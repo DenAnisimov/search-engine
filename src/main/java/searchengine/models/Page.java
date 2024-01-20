251671891d13f842b3fcb4ee97dff15ab9d6a859
Denis Anisimov <den.anisimov.94@mail.ru>
2024-01-20 17:07:12 +0300

    Изменил аннотацию

--- src/main/java/searchengine/models/Page.java
+++ src/main/java/searchengine/models/Page.java
@@ -3,12 +3,14 @@ package searchengine.models;
 import jakarta.persistence.Index;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
-@Setter
+@SuperBuilder
 @Table(name = "pages", indexes = @Index(name = "path_index", columnList = "path", unique = true))
+@RequiredArgsConstructor
 public class Page {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
