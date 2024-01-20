09ea914033bbb20313d64eb90abed131a5b8b560
Denis Anisimov <den.anisimov.94@mail.ru>
2024-01-20 17:09:39 +0300

    Полностью переработал класс

--- src/main/java/searchengine/utils/SiteCrawl.java
+++ src/main/java/searchengine/utils/SiteCrawl.java
@@ -5,60 +5,63 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import searchengine.models.Page;
-import searchengine.models.Site;
-import searchengine.repository.PageRepository;
+import searchengine.dto.page.PageDTO;
+import searchengine.dto.site.SiteDTO;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.RecursiveAction;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Component
 public class SiteCrawl extends RecursiveAction {
-    @Autowired
-    private PageRepository pageRepository;
+    private static Queue<PageDTO> pageDTOQueue = new ConcurrentLinkedQueue<>();
+    private static boolean crawling = true;
     private String rootLink;
-    private Site site;
-    private static HashSet<String> hrefs = new HashSet<>();
+    private SiteDTO siteDTO;
+    public static HashSet<String> hrefs = new HashSet<>();
     private List<SiteCrawl> tasks;
 
-    public SiteCrawl(Site site, String rootLink) {
+
+    public SiteCrawl(SiteDTO siteDTO, String rootLink) {
         tasks = new ArrayList<>();
-        this.site = site;
+        this.siteDTO = siteDTO;
 
         this.rootLink = rootLink;
     }
 
-    private Document dataToDocument(String path) {
+    @Override
+    protected void compute() {
         try {
-            Document document = Jsoup.connect(path).get();
-            return document;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return null;
+            if (!crawling) {
+                return;
+            }
+            Document document = Jsoup.connect(rootLink)
+                    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT " +
+                            "5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                    .referrer("http://www.google.com")
+                    .get();
+            if (getStatusCode(rootLink) == 200) {
+
+                processDocument(document);
+            }
+        } catch (IOException ioe){
+            ioe.printStackTrace();
         }
     }
 
-    @Override
-    protected void compute() {
+    private void processDocument(Document document) {
+        if (document == null) {
+            return;
+        }
 
-        Document document = dataToDocument(rootLink);
         Elements links = document.select("a");
 
-        Page page = new Page();
-        page.setSite(site);
-        page.setCode(getStatusCode(rootLink));
-        page.setContent(document.text());
-        page.setPath(rootLink);
-        pageRepository.save(page);
-
-        Pattern partialLink = Pattern.compile("^[\\/][a-zA-z \\/]{1,}[\\/]$");
+        Pattern partialLink = Pattern.compile("^[\\/][a-zA-z \\/]{1,}[\\/]$|^[\\/][a-zA-z \\/]{1,}\\.html$");
 
         Matcher matcherPartialLink;
 
@@ -69,30 +72,50 @@ public class SiteCrawl extends RecursiveAction {
 
             if (matcherPartialLink.find() && !hrefs.contains(href)) {
 
-                Pattern patternOriginalLink = Pattern.compile("^[https:]{0,}[\\/]{2}[a-z]{1,}[.][a-z]{2,}");
+                Pattern patternOriginalLink = Pattern.compile("^[https:]{0,}[\\/]{2}[a-z.]{1,}[a-z]{2,}");
                 Matcher matcherOriginalLink = patternOriginalLink.matcher(rootLink);
 
                 if (matcherOriginalLink.find()) {
                     String originalLink = rootLink.substring(matcherOriginalLink.start(), matcherOriginalLink.end());
                     hrefs.add(href);
+                    savePageData(document, originalLink + href);
 
-                    SiteCrawl task = new SiteCrawl(site,originalLink + href);
-                    tasks.add(task);
-                    task.fork();
-                    task.join();
+                    createAndForkNewTask(originalLink + href);
                 }
             }
         }
     }
 
-    private int getStatusCode(String path){
+    private void savePageData(Document document, String path) {
+        PageDTO pageDTO = PageDTO.builder()
+                .siteDTO(siteDTO)
+                .code(getStatusCode(path))
+                .content(document.text())
+                .path(path)
+                .build();
+        pageDTOQueue.add(pageDTO);
+    }
+
+    private void createAndForkNewTask(String href) {
+        hrefs.add(href);
+        SiteCrawl task = new SiteCrawl(siteDTO, href);
+        task.fork();
+    }
+
+    private int getStatusCode(String path) {
         try {
-            return Jsoup.connect(path)
-                    .method(Connection.Method.GET)
-                    .execute().statusCode();
-        } catch (IOException e) {
+            return Jsoup.connect(path).method(Connection.Method.GET).execute().statusCode();
+        } catch (Exception e) {
             e.printStackTrace();
             return 0;
         }
+    }
+
+    public static Queue<PageDTO> getPageDTOQueue() {
+        return pageDTOQueue;
+    }
+
+    public static void stopCrawling() {
+        crawling = false;
     }
 }
