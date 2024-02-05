41d54da24ce972697254b8c3ff81469637bd1948
Denis Anisimov <den.anisimov.94@mail.ru>
2024-02-05 18:37:24 +0300

    Добавил метод для поиска по пути страницы

--- src/main/java/searchengine/repository/PageRepository.java
+++ src/main/java/searchengine/repository/PageRepository.java
@@ -2,9 +2,11 @@ package searchengine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import searchengine.models.Page;
+import searchengine.models.Site;
 
 import java.util.List;
 
 public interface PageRepository extends JpaRepository<Page, Integer> {
-    List<Page> findAllBySiteId(int siteId);
+    Page findByPath(String path);
+    List<Page> findAllBySite(Site site);
 }
