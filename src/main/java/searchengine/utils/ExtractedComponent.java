61f2298078bd6fe66de33d1cfea69c2a7770bbd4
Denis Anisimov <den.anisimov.94@mail.ru>
2024-02-13 19:39:19 +0300

    Изменил все сущности на DTO

--- src/main/java/searchengine/utils/ExtractedComponent.java
+++ src/main/java/searchengine/utils/ExtractedComponent.java
@@ -2,40 +2,41 @@ package searchengine.utils;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import searchengine.models.Index;
-import searchengine.models.Lemma;
-import searchengine.models.Page;
+import searchengine.dto.index.IndexDTO;
+import searchengine.dto.lemma.LemmaDTO;
+import searchengine.dto.page.PageDTO;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+
 @RequiredArgsConstructor
 @Component
 public class ExtractedComponent {
     private final LemmaFinderComponent lemmaFinderComponent;
-    public List<Index> getIndexes(List<Lemma> lemmas, Page page) {
-        List<Index> indexes = new LinkedList<>();
+    public List<IndexDTO> getIndexes(List<LemmaDTO> lemmas, PageDTO page) {
+        List<IndexDTO> indexes = new LinkedList<>();
 
-        for (Lemma lemma : lemmas) {
-            Index index = Index.builder()
-                    .lemmaRank(lemma.getFrequency())
-                    .page(page)
-                    .lemma(lemma)
-                    .build();
+        for (LemmaDTO lemmaToAdd : lemmas) {
+                    IndexDTO index = IndexDTO.builder()
+                            .lemmaRank(lemmaToAdd.getFrequency())
+                            .pageDTO(page)
+                            .lemmaDTO(lemmaToAdd)
+                            .build();
 
-            indexes.add(index);
-        }
+                    indexes.add(index);
+            }
 
         return indexes;
     }
 
-    public List<Lemma> getLemmas(Page page) {
-        LinkedList<Lemma> lemmas= new LinkedList<>();
+    public List<LemmaDTO> getLemmas(PageDTO page) {
+        LinkedList<LemmaDTO> lemmas = new LinkedList<>();
 
         for (Map.Entry<String, Integer> pageLemmas : lemmaFinderComponent.main(page.getContent()).entrySet()) {
-            Lemma lemma = Lemma.builder()
-                    .site(page.getSite())
+            LemmaDTO lemma = LemmaDTO.builder()
+                    .siteDTO(page.getSiteDTO())
                     .lemma(pageLemmas.getKey())
                     .frequency(pageLemmas.getValue())
                     .build();
