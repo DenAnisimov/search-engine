34797b98a7ff4a35a93cf7e888a2de61359425a8
Denis Anisimov <den.anisimov.94@mail.ru>
2024-02-13 19:42:58 +0300

    Поменял int на Integer

--- src/main/java/searchengine/dto/lemma/LemmaDTO.java
+++ src/main/java/searchengine/dto/lemma/LemmaDTO.java
@@ -3,19 +3,27 @@ package searchengine.dto.lemma;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import searchengine.dto.site.SiteDTO;
+import searchengine.models.Lemma;
 import searchengine.models.Site;
 
 @Getter
 @SuperBuilder
+@Setter
 @RequiredArgsConstructor
 public class LemmaDTO {
-    private int id;
+    private Integer id;
 
     private SiteDTO siteDTO;
 
     private String lemma;
 
     private int frequency;
+
+    /* todo: Дописать логику*/
+    private boolean isLemmaBelongToSite(Lemma lemma) {
+        return false;
+    }
 }
