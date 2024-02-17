a75b0179ed4087fed27c255e353debeb347f78b0
DenAnisimov <den.anisimov.2206@mail.ru>
2024-02-17 18:41:16 +0300

    Изменил название переменной

--- src/main/java/searchengine/dto/search/DetailedSearchItem.java
+++ src/main/java/searchengine/dto/search/DetailedSearchItem.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class DetailedSearchItem {
     private String site;
     private String siteName;
-    private String url;
+    private String uri;
     private String title;
     private String snippet;
     private Double relevance;
